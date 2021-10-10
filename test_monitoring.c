0bd41440286efbc5fbc15913265ea56aafe1b922
Jessica Feder <jessica.feder@student.kyh.se>
2021-10-10 17:40:39 +0200

    Small changes in code

--- test_monitoring.c
+++ test_monitoring.c
@@ -6,19 +6,15 @@ void write_file();
 
 void delay(int number_of_seconds)
 {
-    // Converting time into milli_seconds
     int milli_seconds = 1000 * number_of_seconds;
-  
-    // Storing start time
     clock_t start_time = clock();
-  
-    // looping till required time is not achieved
     while (clock() < start_time + milli_seconds)
         ;
 }
 
 void main(){
 
+    //Keeping program running in background
     while(1==1){
         write_file();
         delay(1);
@@ -26,17 +22,17 @@ void main(){
 }
 
 void write_file() {
-   FILE *fp = fopen("sensor_data.txt", "w");
 
+   FILE *fp = fopen("sensor_data.txt", "w");
    if (fp == NULL){
        return;
    }
 
-
+//Loop through 236 lines, adding 1 sensor and ;
    for (int i = 0; i < 236; i++) {
        fprintf(fp, "%d", i);
        fprintf(fp, ";");
-       
+//While looping, if random int is odd or even printing out true or false     
         if (rand() % 2 == 0)
                 fprintf(fp, "true;");
             else
